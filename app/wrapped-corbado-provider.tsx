eb9813cdb27b45499f8a5eaf51d25d321136c262
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-05-23 15:20:10 +0200

    Automated

--- app/wrapped-corbado-provider.tsx
+++ app/wrapped-corbado-provider.tsx
@@ -2,17 +2,39 @@
 
 import { CorbadoProvider } from "@corbado/react";
 import englishTranslations from "@/lib/corbado-translations";
+import React, { useEffect, useRef } from "react";
+import { sendEvent, TelemetryEventType } from "@corbado/shared-util";
 
 export default function WrappedCorbadoProvider({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const hasSentTelemetry = useRef(false);
+
     // obtain the Corbado project ID from the environment variables
     const projectId = process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID;
+
+    useEffect(() => {
+        if (hasSentTelemetry.current || !projectId) return;
+
+        void sendEvent({
+            type: TelemetryEventType.EXAMPLE_APPLICATION_OPENED,
+            payload: {
+                exampleName: "corbado/example-ts-nextjs",
+            },
+            sdkVersion: "3.1.0",
+            sdkName: "React SDK",
+            identifier: projectId,
+        });
+
+        hasSentTelemetry.current = true;
+    }, [projectId]);
+
     if (!projectId) {
         throw new Error("Missing Corbado project ID");
     }
+
     return (
         <CorbadoProvider
             projectId={projectId}
