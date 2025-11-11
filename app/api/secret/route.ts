e5618ba4d2f6eac3782ec28e52f0a7a8a5456d26
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-11-11 11:06:02 +0100

    Automated

--- app/api/secret/route.ts
+++ app/api/secret/route.ts
@@ -1,16 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import {
-    getAuthenticatedUserFromAuthorizationHeader,
-    getAuthenticatedUserFromCookie,
-} from "@/lib/server/authentication";
+import { getAuthenticatedUserFromAuthorizationHeader } from "@/lib/server/authentication";
 
 const secretString = "Passkeys are cool!";
 
 export async function GET(req: NextRequest) {
-    // obtain the session token from the request's cookie or authorization header
-    let user = await getAuthenticatedUserFromCookie();
-    user ??= await getAuthenticatedUserFromAuthorizationHeader(req);
-
+    // obtain the session token from the request's authorization header
+    const user = await getAuthenticatedUserFromAuthorizationHeader(req);
     if (!user) {
         return NextResponse.json("Unauthorized", { status: 401 });
     }
